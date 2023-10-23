ed5618749cc4a49a4039c88db4cd150546560467
yl22iee <yl22iee7@gamil.com>
2023-10-23 23:03:32 +0900

    Make Menu

--- src/PracticeEX/components/EventPr.js
+++ src/PracticeEX/components/EventPr.js
@@ -1,38 +1,84 @@
 import { useState } from "react";
 import { styled } from "styled-components";
 
-const Box = styled.div`
-  width: 300px;
-  height: 300px;
-  background-color: ${(props) => props.$bg};
-  border: 3px solid black;
+const Wrap = styled.div`
+  max-width: 450px;
+  width: 100%;
+  height: 100vh;
+  border: 1px solid #eee;
+  margin: 0 auto;
 `;
 
-const ColorBox = styled.div`
-  width: 300px;
-  height: 300px;
-  background-color: ${(props) => props.$hello};
+const MHeader = styled.header`
+  padding: 20px;
+  display: flex;
+  justify-content: space-between;
+  border: 1px solid #eee;
+  position: relative;
+`;
+
+const Logo = styled.div`
+  font-size: 20px;
+  font-weight: 900;
+  text-transform: uppercase;
+`;
+
+const MenuBtn = styled.div`
+  font-size: 20px;
+  font-weight: 900;
+  cursor: pointer;
+  z-index: 10;
+`;
+
+const MenuWrap = styled.ul`
+  position: absolute;
+  top: 0;
+  left: ${(props) => props.$HandleLeft};
+  max-width: 450px;
+  width: 100%;
+  height: 100vh;
+  background-color: lightblue;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  transition: 0.5s;
+  li {
+    margin-bottom: 20px;
+    font-size: 50px;
+    font-weight: 900;
+    text-transform: uppercase;
+  }
 `;
 
 export const EventPr = () => {
-  const [bgColor, setColor] = useState("royalblue");
+  const [Left, setLeft] = useState(0);
   const [Num, setNum] = useState(0);
-  const ChColor = "salmon";
-
-  const ClickBox = () => {
+  const ClickBtn = () => {
     if (Num === 0) {
-      setColor("salmon");
+      setLeft("0%");
       setNum(Num + 1);
     } else if (Num === 1) {
-      setColor("royalblue");
+      setLeft("100%");
       setNum(Num - 1);
     }
   };
 
   return (
     <div>
-      <Box $bg={bgColor} onClick={ClickBox}></Box>
-      <ColorBox $hello={ChColor}></ColorBox>
+      <Wrap>
+        <MHeader>
+          <Logo>logo</Logo>
+          <MenuBtn>=</MenuBtn>
+
+          <MenuWrap $HandleLeft={Left} onClick={ClickBtn}>
+            <li>menu</li>
+            <li>menu</li>
+            <li>menu</li>
+            <li>menu</li>
+          </MenuWrap>
+        </MHeader>
+      </Wrap>
     </div>
   );
 };
